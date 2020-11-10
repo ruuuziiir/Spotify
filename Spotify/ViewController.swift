9ee9c1c7f19e80d08963a5c8c0357fc2ae9c0911
7 <ruuuziiir@gmail.com>
2020-11-10 18:14:28 +0800

    Initial Commit

---
+++ Spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by 7 on 2020/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
